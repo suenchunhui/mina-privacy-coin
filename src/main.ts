2660c3f54c89aaa41752f1279a9957b32a67f7d8
Suen Chun Hui <23443533+suenchunhui@users.noreply.github.com>
2023-10-13 21:58:34 +0800

    cleanup test code

--- src/main.ts
+++ src/main.ts
@@ -44,65 +44,6 @@ function privateLeaf(
   return Poseidon.hash([left, right]);
 }
 
-function maybeSwap(b: Bool, x: Field, y: Field): [Field, Field] {
-  // if(b.equals(Bool(true))){
-  //   return [x, y];
-  // }else{
-  //   return [y, x];
-  // }
-  let m = b.toField().mul(x.sub(y)); // b*(x - y)
-  const x_ = y.add(m); // y + b*(x - y)
-  const y_ = x.sub(m); // x - b*(x - y) = x + b*(y - x)
-  return [x_, y_];
-}
-
-function nextHash(isLeft: Bool, hash: Field, nextHash: Field) {
-  const [left1, right1] = maybeSwap(isLeft, hash, nextHash);
-  return Poseidon.hash([left1, right1]);
-}
-
-function calculateUpdate2Roots(
-  witness1: MerkleWitness32,
-  witness2: MerkleWitness32,
-  leaf1: Field,
-  leaf2: Field
-): Field {
-  let n = witness1.height();
-  let hash1 = leaf1;
-  let hash2 = leaf2;
-
-  let idx1 = witness1.calculateIndex();
-  let idx2 = witness2.calculateIndex();
-
-  //  console.log('d1 ', idx1, idx2);
-
-  for (let i = 1; i < n; ++i) {
-    let idx1_next = idx1.div(Field(2));
-    let idx2_next = idx2.div(Field(2));
-
-    if (idx1_next.equals(idx2_next)) {
-      hash2 = hash1 = Poseidon.hash([hash1, hash2]);
-    } else {
-      hash1 = nextHash(witness1.isLeft[i - 1], hash1, witness1.path[i - 1]);
-      hash2 = nextHash(witness2.isLeft[i - 1], hash2, witness2.path[i - 1]);
-    }
-    // hash1 = Circuit.if(
-    //   idx1_next.equals(idx2_next),
-    //   Poseidon.hash([hash1, hash2]),  //2 branches merged   FIXME need to swap order if idx2 < idx1
-    //   nextHash(witness1.isLeft[i - 1], hash1, witness1.path[i - 1])  //unmerged
-    // );
-    // hash2 = Circuit.if(
-    //   idx1_next.equals(idx2_next),
-    //   Poseidon.hash([hash1, hash2]),  //2 branches merged   FIXME need to swap order if idx2 < idx1
-    //   nextHash(witness2.isLeft[i - 1], hash2, witness2.path[i - 1])  //unmerged
-    // );
-
-    idx1 = idx1_next;
-    idx2 = idx2_next;
-  }
-  return hash1;
-}
-
 //tree defn
 const height = 32;
 class MerkleWitness32 extends MerkleWitness(height) {}
@@ -227,55 +168,6 @@ const txn3 = await Mina.transaction(senderAccount, () => {
 await txn3.prove();
 await txn3.sign([senderKey]).send();
 
-/*
-//events
-//console.log(JSON.stringify(txn3.transaction.accountUpdates[0].body.events));
-// {"hash":"8420083281727309776268175983587936228533289388643792574454353586744076044682","data":[["0","16653878613474267232387783887715849643420830000568047238359874635599369047919"],["1","6"],["0","1252359999092379544927346963835485216706032159767292630364506968907055786443"],["1","2"]]}
-const t3_sender_update_idx =
-  txn3.transaction.accountUpdates[0].body.events.data[3][1];
-const t3_sender_update_data =
-  txn3.transaction.accountUpdates[0].body.events.data[2][1];
-const t3_recipient_update_idx =
-  txn3.transaction.accountUpdates[0].body.events.data[1][1];
-const t3_recipient_update_data =
-  txn3.transaction.accountUpdates[0].body.events.data[0][1];
-
-//update off-chain tree
-const t3_sender_update_data_offline = publicLeaf(
-  user1_pk,
-  mint_amt.sub(tx3_transfer_amt)
-);
-const t3_recipient_update_data_offline = publicLeaf(user2_pk, tx3_transfer_amt);
-
-console.log(
-  `sender update:    index: ${t3_sender_update_idx} data: ${t3_sender_update_data}`
-);
-console.log(
-  `sender data computation:         ${t3_sender_update_data_offline}`
-);
-
-console.log(
-  `recipient update: index: ${t3_recipient_update_idx} data: ${t3_recipient_update_data}`
-);
-console.log(
-  `recipient data computation:      ${t3_recipient_update_data_offline}`
-);
-
-publicTree.setLeaf(user1_idx, t3_sender_update_data_offline);
-publicTree.setLeaf(user2_idx, t3_recipient_update_data_offline);
-
-//compare merkle root
-const newRoot3 = calculateUpdate2Roots(
-  senderWitness,
-  recipientWitness,
-  t3_sender_update_data_offline,
-  t3_recipient_update_data_offline
-);
-// console.log(senderWitness.calculateIndex().toString());
-// console.log(recipientWitness.calculateIndex().toString());
-// console.log(newRoot3.toString());
-*/
-
 //update off-chain tree
 const t3_sender_update_data_offline = publicLeaf(
   user1_pk,
@@ -291,18 +183,6 @@ console.log('tree root after txn3:', publicTreeRoot3.toString());
 
 let user1_bal = mint_amt.sub(tx3_transfer_amt);
 let user2_bal = Field(tx3_transfer_amt);
-
-// try {
-//   const txn2 = await Mina.transaction(senderAccount, () => {
-//     zkAppInstance.update(Field(75));
-//   });
-//   await txn2.prove();
-//   await txn2.sign([senderKey]).send();
-// } catch (ex: any) {
-//   console.log(ex.message);
-// }
-// const num2 = zkAppInstance.num.get();
-// console.log('state after txn2:', num2.toString());
 
 // ----------------------- tx4 init private -----------------------------
 
