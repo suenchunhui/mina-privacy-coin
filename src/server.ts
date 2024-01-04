2ffd1c98c277391ba115d64faf6d239d7bf960b5
Suen Chun Hui <23443533+suenchunhui@users.noreply.github.com>
2024-01-04 12:45:17 +0800

    update server.js to query nullifier tree

--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,8 @@ import express, { Express, Request, Response } from 'express';
 import { Coin } from './Coin.js';
 import {
   Field,
-  Mina,
-  PrivateKey,
-  AccountUpdate,
   MerkleTree,
-  MerkleWitness,
-  PublicKey,
-  Poseidon,
-  Bool,
+  MerkleMap,
   UInt32,
 } from 'o1js';
 
@@ -21,6 +15,7 @@ class MerkleListener {
   coinInstance: Coin;
   publicTree: MerkleTree;
   privateTree: MerkleTree;
+  nullifierTree: MerkleMap;
   lastFetched: UInt32 = UInt32.from(0);
   server;
 
@@ -28,12 +23,13 @@ class MerkleListener {
     this.coinInstance = inst;
     this.publicTree = new MerkleTree(height);
     this.privateTree = new MerkleTree(height);
+    this.nullifierTree = new MerkleMap();
 
     //express server to return tree root and witness using rest api
     if (serverPort > 0) {
       let app = express();
       app.get('/:tree/:fn', (req: Request, res: Response) => {
-        let tree: MerkleTree;
+        let tree: MerkleTree | MerkleMap;
         switch (req.params.tree) {
           case 'public':
             tree = this.publicTree;
@@ -41,7 +37,11 @@ class MerkleListener {
           case 'private':
             tree = this.privateTree;
             break;
-          default:
+          case 'nullifier':
+            tree = this.nullifierTree;
+            break;
+  
+            default:
             throw Error('Undefined tree selector');
         }
 
@@ -51,8 +51,12 @@ class MerkleListener {
             break;
           case 'witness':
             if (req.query.index) {
-              let w = tree.getWitness(BigInt(req.query.index.toString()));
-              //let output = '';
+              let w;
+              if(tree instanceof MerkleTree){
+                w = tree.getWitness(BigInt(req.query.index.toString()));
+              }else{
+                w = tree.getWitness(Field(req.query.index.toString()));
+              }
               res.send(JSON.stringify(w));
             } else {
               throw Error('Missing witness index');
@@ -76,53 +80,47 @@ class MerkleListener {
     );
 
     let public_leaf: Field = Field(-1);
-    let public_index = -1n;
     let private_leaf: Field = Field(-1);
-    let private_index = -1n;
 
     //process events
     //console.log(events);
     events.forEach((e) => {
-      if (e.blockHeight > this.lastFetched) this.lastFetched = e.blockHeight;
-
-      //console.log(e.event.data, e.event.transactionInfo, e.type);
-      //console.log(e.event.data.toFields(0)[0]);
+      if (e.blockHeight > this.lastFetched) 
+        this.lastFetched = e.blockHeight;
 
       switch (e.type) {
         case 'update-public-leaf':
           public_leaf = e.event.data.toFields(0)[0];
           break;
         case 'update-public-leaf-index':
-          public_index = e.event.data.toFields(0)[0].toBigInt();
+          let public_index = e.event.data.toFields(0)[0].toBigInt();
+          if (public_index != -1n && public_leaf != Field(-1)) {
+            this.publicTree.setLeaf(public_index, public_leaf);
+            public_leaf = Field(-1);
+          }
           break;
         case 'update-private-leaf':
           private_leaf = e.event.data.toFields(0)[0];
           break;
         case 'update-private-leaf-index':
-          private_index = e.event.data.toFields(0)[0].toBigInt();
+          let private_index = e.event.data.toFields(0)[0].toBigInt();
+          if (private_index != -1n && private_leaf != Field(-1)) {
+            this.privateTree.setLeaf(private_index, private_leaf);
+            private_leaf = Field(-1);
+          }
+          break;
+        case 'update-nullifier-leaf-index':
+          let nullifier_index = e.event.data.toFields(0)[0].toBigInt();
+          this.nullifierTree.set(Field(nullifier_index), Field(1));
           break;
       }
     });
-
-    //update tree(s)
-    if (public_index != -1n && public_leaf != Field(-1)) {
-      this.publicTree.setLeaf(public_index, public_leaf);
-      console.log(
-        `  Updating public tree: index=${public_index} value=${public_leaf}`
-      );
-    }
-    if (private_index != -1n && private_leaf != Field(-1)) {
-      this.privateTree.setLeaf(private_index, private_leaf);
-      console.log(
-        `  Updating private tree: index=${private_index} value=${private_leaf}`
-      );
-    }
   }
 
   shutdown() {
     if (this.server) {
       this.server.close(() => {
-        console.log('Server closed');
+        //console.log('Server closed');
       });
     }
   }
